73017489f14c532d4e22414b77880bb9761a532f
Rodrigo <rodrigo.senacbeb@gmail.com>
2019-08-12 11:48:05 -0300

    style: Página inicial finalizada

---
+++ js/filtro_pesquisa.js
@@ -0,0 +1,9 @@
+function exibirFiltros(elemento){
+    var display = document.getElementById(elemento).style.display;
+
+if(display == "none")
+    document.getElementById(elemento).style.display = 'block';
+else
+    document.getElementById(elemento).style.display = 'none';
+}
+
