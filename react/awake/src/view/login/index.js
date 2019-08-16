b9a414213ee4851cc2b305670541778a002a1226
RODRIGO GONCALVES SANTANA <rodrigo.gsantana1@senacsp.edu.br>
2019-08-16 08:37:40 -0300

    style: WIP - finalizando visual da tela de login

--- react/awake/src/view/login/index.js
+++ react/awake/src/view/login/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './login.css';
 
+import Logo from '../../img/happy.png';
+
 function Login(){
     return(
-        <div className="login-content">
+        <div className="login-content d-flex align-items-center">
             <form className="form-signin mx-auto text-center">
-              <img className="mb-4" src="" alt="Logo" width="72" height="72" />
+              <img className="mb-4" src={Logo} alt="Logo" width="72" height="72" />
               <h1 class="h3 mb-3 font-weight-normal">Login</h1>
   
               <input type="email" className="form-control" placeholder="Email" autofocus />
