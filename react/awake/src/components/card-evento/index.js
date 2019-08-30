ad9dce935664fc2c0ceb36aea23be538082a70b2
RODRIGO GONCALVES SANTANA <rodrigo.gsantana1@senacsp.edu.br>
2019-08-30 10:37:09 -0300

    feat: listar eventos cadastrados

--- react/awake/src/components/card-evento/index.js
+++ react/awake/src/components/card-evento/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './card-evento.css';
 
-function EventoCard(){
+function EventoCard({id, img, titulo, detalhes, visualizacoes}){
     return(
         <div className="col-md-3 col-sm-12">
             <img src="https://via.placeholder.com/150x50" className="card-img-top img-cartao"/>
             <div className="card-body">
-                <h5>Titulo</h5>
-                <p className="card-text text-justify">Detalhes</p>
+                <h5>{titulo}</h5>
+                <p className="card-text text-justify">{detalhes}</p>
 
                 <div className="row rodape-card d-flex align-items-center">
                     <div className="col-6">
@@ -17,7 +17,7 @@ function EventoCard(){
                         </Link>
                     </div>
                     <div className="col-6 text-right">
-                        <i className="fas fa-eye"></i> <span>2019</span>
+                        <i className="fas fa-eye"></i> <span>{visualizacoes}</span>
                     </div>
                 </div>
             </div>
--- react/awake/src/view/home/index.js
+++ react/awake/src/view/home/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import { useSelector} from 'react-redux';
 import EventoCard from '../../components/card-evento/';
+import firebase from '../../config/firebase';
 
 /*Componentes Personalizados*/
 import Navbar from '../../components/navbar/';
 
 function Home(){
+    const listaeventos = [];
+    const [eventos, setEventos] = useState();
+
+    useEffect(() => {
+        firebase.firestore().collection('eventos').get().then(async resultado => {
+            await resultado.docs.forEach(doc => {
+                listaeventos.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })            
+            setEventos(listaeventos);        
+        });        
+    });
+
+
     return(
         <>
         <Navbar />
@@ -21,21 +38,16 @@ function Home(){
             </div>
 
             <div className="row">
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />        
-                <EventoCard />            
+                {
+                   eventos && eventos.map(item => <EventoCard 
+                                                    key={item.id} 
+                                                    id={item.id}
+                                                    img={item.foto}
+                                                    titulo={item.titulo}
+                                                    detalhes={item.detalhes}
+                                                    visualizacoes={item.visualizacoes}
+                                                    />)                                
+                }
             </div>
         </div>
         </>
