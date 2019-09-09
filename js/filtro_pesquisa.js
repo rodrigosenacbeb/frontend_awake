368ed13d2aec37dd4b37296a7fc65bc39fd880eb
RODRIGO GONCALVES SANTANA <rodrigo.gsantana1@senacsp.edu.br>
2019-09-09 09:17:04 -0300

    feature: botão de compartilhar evento

--- js/filtro_pesquisa.js
+++
@@ -1,9 +0,0 @@
-function exibirFiltros(elemento){
-    var display = document.getElementById(elemento).style.display;
-
-if(display == "none")
-    document.getElementById(elemento).style.display = 'block';
-else
-    document.getElementById(elemento).style.display = 'none';
-}
-
